0d70791e6faa5ba5ba1dc0eb2fce5789e6a2c70a
Krystian Paszek <krystian.paszek@offerista.com>
2020-08-01 12:34:17 +0200

    Add LanguagesList to the project

---
+++ Flashcards/LanguagesList.swift
@@ -0,0 +1,26 @@
+//
+//  LanguagesList.swift
+//  Flashcards
+//
+//  Created by Krystian Paszek on 01/08/2020.
+//  Copyright © 2020 Krystian Paszek. All rights reserved.
+//
+
+import SwiftUI
+
+struct LanguagesList: View {
+    @Environment(\.managedObjectContext) var context
+    @FetchRequest(entity: FCDLanguage.entity(), sortDescriptors: [NSSortDescriptor(key: "name", ascending: true)]) var languages: FetchedResults<FCDLanguage>
+
+    var body: some View {
+        List(languages) { object in
+            Text((object as! FCDLanguage).name!)
+        }
+    }
+}
+
+struct LanguagesList_Previews: PreviewProvider {
+    static var previews: some View {
+        LanguagesList()
+    }
+}
