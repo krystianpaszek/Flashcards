a9d43df7adb1f59182586044b755a3ae6ddef5c4
Krystian Paszek <krystian.paszek@offerista.com>
2020-08-01 13:36:31 +0200

    Add HomescreenView to the project

---
+++ Flashcards/HomescreenView.swift
@@ -0,0 +1,21 @@
+//
+//  HomescreenView.swift
+//  Flashcards
+//
+//  Created by Krystian Paszek on 01/08/2020.
+//  Copyright © 2020 Krystian Paszek. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomescreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomescreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomescreenView()
+    }
+}
