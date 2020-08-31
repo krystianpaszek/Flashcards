459364a0a1b4b4e795a5020fb6bd4c4c72f3100c
Krystian Paszek <krystian.paszek@offerista.com>
2020-09-01 00:09:35 +0200

    Set filename as a category's name when importing a file

--- Flashcards/Data Management/FileImporter.swift
+++ Flashcards/Data Management/FileImporter.swift
@@ -19,14 +19,17 @@ struct FileImporter {
 
     func load(contentsOf url: URL) throws {
         let data = try Data(contentsOf: url)
+        let filename = url.lastPathComponent
         let text = String(data: data, encoding: .utf8)!
         let parser = TestYourEnglishFormatParser()
         let items = parser.parse(rawText: text)
 
-        items.forEach { item in
-            guard let originalWord = dataFactory.addWord(spelling: item.originalSpelling, languageCode: "de") else { return }
-            guard let translatedWord = dataFactory.addWord(spelling: item.translation, languageCode: "pl") else { return }
-            dataFactory.addFlashcard(firstWord: originalWord, secondWord: translatedWord)
-        }
+        let flashcards = items.map { item -> FCDFlaschard? in
+            guard let originalWord = dataFactory.addWord(spelling: item.originalSpelling, languageCode: "de") else { return nil }
+            guard let translatedWord = dataFactory.addWord(spelling: item.translation, languageCode: "pl") else { return nil }
+            return dataFactory.addFlashcard(firstWord: originalWord, secondWord: translatedWord)!
+        }.compactMap { $0 }
+
+        dataFactory.addCategory(name: filename, flashcards: flashcards)
     }
 }
